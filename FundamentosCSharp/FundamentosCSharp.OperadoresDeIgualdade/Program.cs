737339cadc6ac3b17352d3920192c4b9c0b49c8e
Matheus <matheus.souza@indikatore.com>
2021-12-23 16:29:57 -0300

    Fundamentos de C#, operadores de igualdade

---
+++ FundamentosCSharp/FundamentosCSharp.OperadoresDeIgualdade/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Utilizando operadores de igualdade: == , !=
+// Esse operador retorna um tipo boleano: (true ou false)
+
+int valorA = 4;
+int valorB = 5;
+
+Console.WriteLine("Bem vindo ao fundamentos de C#, operadores de igualdade!");
+
+// O operador == (igual) representa se o valor A é igual ao valor B 
+Console.WriteLine($"Comparação de valores, 4 é igual a 5 ? = { valorA == valorB}");
+
+// O operador != (diferente) representa se o valor A é diferente do valor B 
+Console.WriteLine($"Comparação de valores, 4 é diferente de 5 ? = { valorA != valorB}");
+
+

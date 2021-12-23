46b7a880cc7e504e605d541ab5c6732cd6ce8665
Matheus <matheus.souza@indikatore.com>
2021-12-23 17:13:35 -0300

    Fundamentos de C#, Estrutura condicional If/Else!

---
+++ FundamentosCSharp/FundamentosCSharp.IfElse/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Estrutura de condição If/Else: Estrutura que atende uma condição e a partir dela o programa consegue tomar um tipo de decisão especifica.
+// If ou else if: É executado quando o bloco de codigo é verdadeiro
+// Else: É executado quando o bloco de codigo é falso
+
+Console.WriteLine("Bem vindo ao fundamentos de C#, Estrutura condicional If/Else!");
+
+Console.WriteLine("Digite a nota do Aluno!");
+
+var nota = Console.ReadLine();
+var notaAluno = Convert.ToInt32(nota);
+
+if (notaAluno >= 7)
+{
+    Console.WriteLine($"Parabens você tirou {notaAluno} pontos, e esta aprovado!");
+
+} else if (notaAluno >= 5)
+{
+    Console.WriteLine($"Devido aos {notaAluno} pontos, você precisara fazer a recuperação!");
+} else
+{
+    Console.WriteLine($"Infelizmente os {notaAluno} pontos não foram suficiente, então esta reprovado!");
+}
+
+
